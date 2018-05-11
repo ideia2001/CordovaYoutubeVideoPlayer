9dafce2a37e1061574143a654b6f5afd7c950942
Lucas Pimenta <lucas.pimenta94@outlook.com>
2018-05-11 09:57:19 -0300

    replace android platform to fix conflict on android <= 4.4

--- platforms/android/src/com/bunkerpalace/cordova/YoutubeVideoPlayer.java
+++ platforms/android/src/com/bunkerpalace/cordova/YoutubeVideoPlayer.java
@@ -1,73 +1,62 @@
 package com.bunkerpalace.cordova;
-
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.ConfigXmlParser;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaPreferences;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.content.Context;
+
 import android.content.Intent;
 import android.net.Uri;
-import com.google.android.youtube.player.YouTubeIntents;
-import com.keyes.youtube.OpenYouTubePlayerActivity;
-import android.os.Build;
 import android.util.Log;
+
+//Importa pacotes para criar a intent do youtube
+import java.lang.Object;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+import com.google.android.youtube.player.YouTubeApiServiceUtil;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import android.os.Build.VERSION_CODES;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
+
+import com.keyes.youtube.OpenYouTubePlayerActivity;
 
 public class YoutubeVideoPlayer extends CordovaPlugin {
 
-	private CallbackContext callbackContext;
-
 	@Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		
+
 		if(action.equals("openVideo")) {
 			String url = args.getString(0);
-        	this.openVideo(url);
-			this.callbackContext = callbackContext;
+        	this.openVideo(url, callbackContext);
         	return true;
         }
+
 		return false;
 	}
 
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		if (requestCode == 242) {
-			if (resultCode == this.cordova.getActivity().RESULT_OK) {
-				this.callbackContext.success();
-			} else {
-				this.callbackContext.error("Error");
-			}
+	private void openVideo(String vid, CallbackContext callbackContext) {
+
+		//Pega a app level do android
+		int SDK_INT = android.os.Build.VERSION.SDK_INT;
+
+		//Chama o service do youtube
+		YouTubeInitializationResult result = YouTubeApiServiceUtil.isYouTubeApiServiceAvailable(this.cordova.getActivity());
+		//Se retornar sucesso
+		if(result == YouTubeInitializationResult.SUCCESS)
+		{
+			//Cria a intent do youtube
+			//o segundo parametro tem que alterar por uma APIKEY do app
+			Intent intent = YouTubeStandalonePlayer.createVideoIntent(this.cordova.getActivity(), "AIzaSyDKUFw8Mm0nlIIMI09PMaLF0jZAV4M0-XE", vid, 0, true, false);
+			this.cordova.getActivity().startActivity(intent);
+		}
+		else if(SDK_INT < 22) //app level 22 é equivalente a versão 5.1 do android
+		{
+			//Toast.makeText(this.cordova.getActivity(),"É necessario o youtube",Toast.LENGTH_SHORT).show();   Essa linha era pra mostrar um alerta
+			Intent intent = new Intent(null, Uri.parse("ytv://"+vid), this.cordova.getActivity(), OpenYouTubePlayerActivity.class);
+			this.cordova.getActivity().startActivity(intent);
 		}
 	}
 
-	private void openVideo(String videoId) {
-		Intent intent = createYoutubeIntent(videoId);
-		cordova.startActivityForResult(this, intent, 242);
-	}
-
-	private Intent createYoutubeIntent(String videoId) {
-		if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP){
-			Intent intent;
-			Context cordovaContext = cordova.getActivity();
-			String version = YouTubeIntents.getInstalledYouTubeVersionName(cordovaContext);
-			if(version.startsWith("11.16") && YouTubeIntents.canResolvePlayVideoIntent(cordovaContext)) {
-				intent = YouTubeIntents.createPlayVideoIntent(cordovaContext, videoId);
-			} else {
-				if(YouTubeIntents.canResolvePlayVideoIntentWithOptions(cordovaContext)){
-					intent = YouTubeIntents.createPlayVideoIntentWithOptions(cordovaContext, videoId, true, true);
-				} else {
-					intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=" + videoId), cordovaContext, YouTubeActivity.class);
-					intent.putExtra("videoId", videoId);
-	                ConfigXmlParser parser = new ConfigXmlParser();
-	                parser.parse(cordovaContext);
-	                CordovaPreferences prefs = parser.getPreferences();
-	                intent.putExtra("YouTubeApiId", prefs.getString("YouTubeDataApiKey","YOUTUBE_API_KEY"));
-				}
-			}
-			return intent;
-		}
-
-		return new Intent(null, Uri.parse("ytv://" + videoId), cordova.getActivity(), OpenYouTubePlayerActivity.class);
-	}
 }
+
+//plugman install --platform android --project "C:\Users\Igor\Desktop\projetoVideoPlayer\platforms\android" --plugin "C:\Users\Igor\Desktop\projetoVideoPlayer\plugins\com.bunkerpalace.cordova.YoutubeVideoPlayer"
